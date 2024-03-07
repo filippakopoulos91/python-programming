beeb2783f629d99220e63146e453c17225527188
Panagiotis Filippakopoulos <filippakpan@gmail.com>
2024-03-07 15:37:22 +0200

    changed the whole program. does not work properly. just a snapshot, so i can continue from here.

--- average grade.py
+++ average grade.py
@@ -1,14 +1,17 @@
-n1 = float(input("Give the first grade: "))
-n2 = float(input("Give the second grade: "))
-n3 = float(input("Give the third grade: "))
-n4 = float(input("Give the fourth grade: "))
-n5 = float(input("Give the fifth grade: "))
-n6 = float(input("Give the sixth grade: "))
-n7 = float(input("Give the seventh grade: "))
-n8 = float(input("Give the eighth grade: "))
-n9 = float(input("Give the ninth grade: "))
-n10 = float(input("Give the tenth grade: "))
+total = 0
+grade = 0
 
-avg = (n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + n10) / 10   #calculates the average grade of the class
+print("Enter ten grades")
 
-print('The average grade in this class is %.1f' % avg)
+for i in range(1, 11):
+    if (grade >= 0) and (grade <= 20):
+        grade = float(input(""))
+        total += grade
+        avg = total / 10 # avg stands for average
+    else:
+        print("Invalid value! Enter again!")
+        grade = float(input(""))
+        total += grade
+        avg = total / 10 
+            
+print("The average grade is %.2f" % avg)   
