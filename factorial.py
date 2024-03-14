a283d6301519188d5872be5dcacfc0e09722f93e
Panagiotis Filippakopoulos <filippakpan@gmail.com>
2024-03-14 12:38:30 +0200

    Created a fuction that prints the factorial of a whole number, using for and while loops inside of if statements.

--- factorial.py
+++ factorial.py
@@ -1,47 +1,27 @@
-n1 = int(input("Give the first integer number: "))
+def factorial():
 
-#number check and factorization of the first given number
+    n = int(input("Number: "))
 
-factorial = 1
+    factorial = 1
 
-if n1 < 0:         
-    print("Negative numbers don't have factorial!")
-elif n1 == 0:
-    print("The factorial of 0 is 1")
-else:
-    for i in range(1, n1 + 1):
-        factorial = factorial * i
-   
-    print("The factorial of", n1, "is", factorial)
+    if n == 0:
+        print("0! = 1")
+    elif n > 0:
+        for i in range(1, n + 1):
+            factorial *= i
+        print(n, "! = ", factorial)
+    else:
+         while n < 0:
+            user_input = input("Negative numbers don't have factorial. Do you want to re-enter? (yes/no): ")
+            if user_input.lower() in ["yes", "y"]:
+                n = int(input("Number: "))
+                if n == 0:
+                    print("0! = 1")
+                elif n > 0:
+                    for i in range(1, n + 1):
+                        factorial *= i
+                    print(n, "! = ", factorial)
+            else:
+                break
 
-n2 = int(input("Give the second integer number: ")) 
-
-#number check and factorization of the second given number
-
-factorial = 1
-
-if n2 < 0:
-    print("Negative numbers don't have factorial!")
-elif n2 == 0:
-    print("The factorial of 0 is 1")
-else:
-    for y in range(1, n2 + 1):
-        factorial = factorial * y
-   
-    print("The factorial of", n2, "is", factorial)
-
-n3 = int(input("Give the third integer number: "))
-
-#number check and factorization of the third given number
-
-factorial = 1
-
-if n3 < 0:
-    print("Negative numbers don't have factorial!")
-elif n3 == 0:
-    print("The factorial of 0 is 1")
-else:
-    for x in range(1, n3 + 1):
-        factorial = factorial * x
-   
-    print("The factorial of", n3, "is", factorial)
+factorial()
